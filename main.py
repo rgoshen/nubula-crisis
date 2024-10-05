c2aeb30d8c1cff2296f4fd9cab803b9b26ecbdab
rgoshen <rick.goshen@gmail.com>
2024-10-05 09:21:39 -0700

    feature: Game initialization
    - add clear function to clear the terminal
    
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import utilities
 import welcome
 
 if __name__ == '__main__':
     welcome.display_game_logo()
-
+    utilities.clear()
---
+++ utilities.py
@@ -0,0 +1,15 @@
+import os
+
+
+def clear():
+    """
+    Clears the terminal screen.
+
+    This function clears the terminal screen, using 'cls' for Windows systems
+    and 'clear' for Unix-based systems (Linux, macOS). It detects the operating system
+    and runs the appropriate command to refresh the display, creating a clean screen for the next output.
+
+    Usage:
+        Call this function to clear the terminal screen when needed.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
