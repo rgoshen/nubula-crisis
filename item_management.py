f31dd4574f2abb9d07c7b30bc45ba613d9a178d7
rgoshen <rick.goshen@gmail.com>
2024-10-11 14:19:17 -0700

    add inventory command for player to see inventory and refactored print_player so it no longer prints out the inventory
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- item_management.py
+++ item_management.py
@@ -17,8 +17,24 @@ def get_item(current_room, rooms, item_name, player_inventory):
         player_inventory.append(rooms[current_room]['Item'])
         # Remove the item from the room
         rooms[current_room]['Item'] = None
-        print(f"You have collected the {item_name}.")
+        print(f"You have collected the {item_name.capitalize()}.")
         return True
     else:
         print("There is no such item in this room or the command is invalid.")
         return False
+
+
+def display_inventory(player_inventory):
+    """
+    Displays the player's inventory.
+
+    Parameters:
+        player_inventory (list): A list containing the player's current items.
+
+    Returns:
+        None
+    """
+    if player_inventory:
+        print(f"Your inventory contains: {', '.join(player_inventory)}")
+    else:
+        print("Your inventory is empty.")
